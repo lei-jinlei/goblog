812daf891b0d887c5d41d2ad64afe6f2318985be
雷金磊 <leijinlei_phper@163.com>
2021-09-15 17:31:15 +0800

    重构中间件

--- main.go
+++ main.go
@@ -1,47 +1,15 @@
 package main
 
 import (
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
-	"goblog/pck/database"
 	"net/http"
-	"strings"
 )
 
-var router *mux.Router
-var db *sql.DB
-
-func forceHTMLMiddleware(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		h.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 除首页以外，移除所有请求路径后面的斜杠
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-
-		// 将请求传递下去
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
-	database.Initialize()
-	db = database.DB
 
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
-
-	// 中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
-
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
